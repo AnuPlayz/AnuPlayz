197e010c06cba7fce834b13b406e7ddfedc9c395
Aniruddh Dubge <120038186+AnuPlayz@users.noreply.github.com>
2023-07-21 06:57:35 +0530

    Create update.js

---
+++ src/update.js
@@ -0,0 +1,98 @@
+const path = require("path");
+const fetch = require("node-fetch");
+const fs = require("fs");
+
+let stars = 0,
+  page = 1;
+
+let special;
+
+const CountStars = async () => {
+  let StarsData = await fetch(
+    `https://api.github.com/users/anuplayz/starred?per_page=100&page=${page}`
+  ).then((res) => res.json());
+  stars += StarsData.length;
+  page++;
+  if (StarsData.length === 100) CountStars();
+  else WriteReadMe();
+};
+
+const WriteReadMe = async () => {
+  //Get ReadMe path
+  const ReadMe = path.join(__dirname, "..", "README.md");
+  const date = new Date();
+  
+  //Season Based Emoji
+  let dd = date.getDate(), mm = date.getMonth() + 1
+  
+  if(mm === 12)special = ["⛄", "❄", "🎄"]
+  else if(mm === 9 && dd === 29) special = ["🎉", "🎈", "🎊"]
+
+  //Fetching Info From Github API
+  let UserData = await fetch("https://api.github.com/users/anuplayz").then(
+    (res) => res.json()
+  );
+
+  //Creating the text what we gonna save on ReadMe file
+  const text = `<h1 align="center">Hi 👋, I'm Aniruddh Dubge</h1>
+<h3 align="center">A passionate frontend developer from India</h3>
+
+<p align="left"> <img src="https://komarev.com/ghpvc/?username=anuplayz&label=Profile%20views&color=0e75b6&style=flat" alt="anuplayz" /> </p>
+
+- 🔭 I’m currently working on **Venti Cards | Blockchain Project**
+
+- 🌱 I’m currently learning **solidity**
+
+- 💬 Ask me about **react , solidity**
+
+- 📫 How to reach me **adubge@gmail.com**
+
+- ⚡ Fun fact **I think I am funny**
+
+<h3 align="left">Connect with me:</h3>
+<p align="left">
+<a href="https://instagram.com/ani__x_" target="blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/instagram.svg" alt="ani__x_" height="30" width="40" /></a>
+</p>
+
+<h3 align="left">Languages and Tools:</h3>
+<p align="left"> <a href="https://www.cprogramming.com/" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/c/c-original.svg" alt="c" width="40" height="40"/> </a> <a href="https://www.w3schools.com/css/" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg" alt="css3" width="40" height="40"/> </a> <a href="https://expressjs.com" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/express/express-original-wordmark.svg" alt="express" width="40" height="40"/> </a> <a href="https://git-scm.com/" target="_blank" rel="noreferrer"> <img src="https://www.vectorlogo.zone/logos/git-scm/git-scm-icon.svg" alt="git" width="40" height="40"/> </a> <a href="https://www.w3.org/html/" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg" alt="html5" width="40" height="40"/> </a> <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg" alt="javascript" width="40" height="40"/> </a> <a href="https://www.mongodb.com/" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg" alt="mongodb" width="40" height="40"/> </a> <a href="https://nodejs.org" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg" alt="nodejs" width="40" height="40"/> </a> <a href="https://reactjs.org/" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg" alt="react" width="40" height="40"/> </a> <a href="https://www.typescriptlang.org/" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-original.svg" alt="typescript" width="40" height="40"/> </a> </p>
+
+<h2 align="center"> 🚀 My Stats 🚀</h2>
+<p align="center">
+<img src="https://github-readme-streak-stats.herokuapp.com/?user=anuplayz&theme=tokyonight">
+</p>
+
+  
+<!-- Last updated on ${date.toString()} ;-;-->
+<i>Last updated on ${date.getDate()}${
+    date.getDate() === 1
+      ? "st"
+      : date.getDate() === 2
+      ? "nd"
+      : date.getDate() === 3
+      ? "rd"
+      : "th"
+  } ${
+    [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ][date.getMonth()]
+  } ${date.getFullYear()} using magic</i> ${special?special[2]:"✨"} ${(mm === 3 && dd === 29)?"and... today is my birthday":""}`;
+
+  //Saving on readme.md
+  fs.writeFileSync(ReadMe, text);
+};
+
+(() => {
+    CountStars();
+})()
